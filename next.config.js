8cbae06b68575ed66e74987c733669fb2983a8f0
arthurvtl <arthur.vitall05@gmail.com>
2026-01-27 17:56:19 -0300

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: { 
     unoptimized: true
   },
+  basePath: '/my-page',
+  assetPrefix: '/my-page/',
   eslint: {
     ignoreDuringBuilds: true,
   },
